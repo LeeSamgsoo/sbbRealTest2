d97adc260c928601e184caec4d80529e9163ae81
Leesamgsoo <sslee3680035@gmail.com>
2024-09-23 08:55:15 +0900

    init

---
+++ src/test/java/com/asdf/qwer/QwerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asdf.qwer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QwerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
