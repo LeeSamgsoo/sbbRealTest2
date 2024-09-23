bc7e19e2a77dd78bc329d0b6a63f3fe58818ad69
Leesamgsoo <sslee3680035@gmail.com>
2024-09-23 10:12:27 +0900

    paging1

--- src/main/java/com/asdf/qwer/article/ArticleController.java
+++ src/main/java/com/asdf/qwer/article/ArticleController.java
@@ -1,9 +1,12 @@
 package com.asdf.qwer.article;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,7 +15,11 @@ public class ArticleController {
     private final ArticleService articleService;
 
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+                       @RequestParam(value = "kw", defaultValue = "") String kw) {
+        Page<Article> paging = this.articleService.getPage(page, kw);
+        model.addAttribute("page", page);
+        model.addAttribute("kw", kw);
         return "article_list";
     }
 }
--- src/main/java/com/asdf/qwer/article/ArticleRepository.java
+++ src/main/java/com/asdf/qwer/article/ArticleRepository.java
@@ -1,6 +1,21 @@
 package com.asdf.qwer.article;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
+    @Query("select "
+            + "distinct A "
+            + "from Article A "
+            + "left outer join SiteUser U on A.author=U "
+            + "where "
+            + "   A.title like %:keyword% "
+            + "   or A.content like %:keyword% "
+            + "   or U.username like %:keyword% ")
+    Page<Article> findAllByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
