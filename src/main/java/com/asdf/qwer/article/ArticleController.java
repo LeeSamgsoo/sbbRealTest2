5ab90722a2f728dc87d13fc45dea887c40f2fa28
Leesamgsoo <sslee3680035@gmail.com>
2024-09-23 10:39:31 +0900

    create atricle

--- src/main/java/com/asdf/qwer/article/ArticleController.java
+++ src/main/java/com/asdf/qwer/article/ArticleController.java
@@ -1,21 +1,26 @@
 package com.asdf.qwer.article;
 
+import com.asdf.qwer.user.SiteUser;
+import com.asdf.qwer.user.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/article")
 public class ArticleController {
     private final ArticleService articleService;
+    private final UserService userService;
 
     @GetMapping("/list")
     public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
@@ -35,5 +40,27 @@ public class ArticleController {
         model.addAttribute("article", article);
         return "article_detail";
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/create")
+    public String create(ArticleForm articleForm) {
+        return "article_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/create")
+    public String create(@Valid ArticleForm articleForm, BindingResult bindingResult,
+                         Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "article_form";
+        }
+        SiteUser user = this.userService.get(principal.getName());
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "생성 권한이 없습니다");
+        }
+        this.articleService.create(articleForm.getTitle(), articleForm.getContent(), user);
+        return "redirect:/article/list";
+    }
+
 
 }
