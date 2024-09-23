95f88618f0e78ab3f89a694ba708a68d56a47c25
Leesamgsoo <sslee3680035@gmail.com>
2024-09-23 09:41:41 +0900

    signup

--- src/main/java/com/asdf/qwer/SecurityConfig.java
+++ src/main/java/com/asdf/qwer/SecurityConfig.java
@@ -6,8 +6,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.thymeleaf.standard.expression.GreaterOrEqualToExpression;
 
 @Configuration
 @EnableWebSecurity
@@ -31,5 +33,10 @@ public class SecurityConfig {
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/asdf/qwer/user/UserController.java
+++ src/main/java/com/asdf/qwer/user/UserController.java
@@ -1,7 +1,12 @@
 package com.asdf.qwer.user;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,4 +14,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
+
+    @GetMapping("/signup")
+    public String signup(UserCreateForm userCreateForm) {
+        return "signup_form";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+        if (!userCreateForm.getPassword().equals(userCreateForm.getPasswordCheck())) {
+            bindingResult.rejectValue("password", "password incorrect", "비밀번호가 다릅니다.");
+            return "signup_form";
+        }
+        try {
+            this.userService.create(userCreateForm.getUsername(), userCreateForm.getPassword(), userCreateForm.getNickname());
+        } catch (DataIntegrityViolationException e) {
+            bindingResult.reject("signup error", "이미 가입된 유저입니다.");
+            return "signup_form";
+        }
+        return "redirect:/article/list";
+    }
 }
